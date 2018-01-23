4e5efeeda51e8ec715b38855e1e8317c0e193368
Nicolas Godbout <nickg73@gmail.com>
2018-01-23 01:39:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,20 +1,5 @@
-Welcome to Tic Tac Toe
-   |   |
+  |   |
 -----------
-   |   |
+  |   |
 -----------
-   |   |
-What move would X like to make (1-9)?
-> 5
-   |   |
------------
-   | X |
------------
-   |   |
-What move would O like to make (1-9)?
-> 1
- 0 |   |
------------
-   | X |
------------
-   |   |   
+  |   |     
